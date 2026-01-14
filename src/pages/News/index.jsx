5efac9b0597510c6a8a0b67589170271a66d2cd3
MuhAdnanCoding <adnanadjah456@gmail.com>
2026-01-14 21:09:05 +0800

    fix minor

--- src/pages/News/index.jsx
+++ src/pages/News/index.jsx
@@ -1,3 +1,23 @@
+import HeaderBack from "@/components/Header/HeaderBack";
+import { useState } from "react";
+
 const News = () => {
-    
-}
+  const [data, setData] = useState([]);
+
+  const fetchData = async () => {
+    const data = JSON.stringify({
+        sort: "-created_at",
+        page: 1
+    })
+
+  }
+  
+  return (
+    <section>
+      <HeaderBack text="News" />
+      <div className="section-box"></div>
+    </section>
+  );
+};
+
+export default News;
