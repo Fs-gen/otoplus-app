88437fc38ac114aa380c03eb5ba4c0feebf6d55e
Fs-Gen <49970510+Fs-gen@users.noreply.github.com>
2026-01-17 14:09:58 +0000

    Add web app manifest for OTOPLUSID
    - Created a new manifest.json file to define the web app's metadata.
    - Included various icon sizes for better display across devices.
    - Set theme and background colors to white.
    - Configured the app to launch in standalone mode with a portrait orientation.

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -3,7 +3,43 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <meta name="application-name" content="OtoplusID" />
+<meta name="apple-mobile-web-app-capable" content="yes" />
+<meta name="apple-mobile-web-app-status-bar-style" content="default" />
+<meta name="apple-mobile-web-app-title" content="OtoplusID" />
+<meta name="description" content="Jual Mobil Daihatsu Dan Dapatkan Reward!" />
+<meta name="format-detection" content="telephone=no" />
+<meta name="mobile-web-app-capable" content="yes" />
+{/* <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+<meta name="msapplication-TileColor" content="#2B5797" />
+<meta name="msapplication-tap-highlight" content="no" /> */}
+<meta name="theme-color" content="#000000" />
+
+<link rel="apple-touch-icon" href="/icons/logo-512.png" />
+<link rel="apple-touch-icon" sizes="152x152" href="/icons/logo-152.png" />
+<link rel="apple-touch-icon" sizes="180x180" href="/icons/logo-180.png" />
+<link rel="apple-touch-icon" sizes="167x167" href="/icons/logo-167.png" />
+
+<link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon.ico" />
+<link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon.ico" />
+<link rel="manifest" href="/manifest.json" />
+<link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
+<link rel="shortcut icon" href="/favicon.ico" />
+<meta name="twitter:card" content="summary" />
+<meta name="twitter:url" content={window.location.origin} />
+<meta name="twitter:title" content="OtoplusID" />
+<meta name="twitter:description" content="Jual Mobil Daihatsu Dan Dapatkan Reward!" />
+<meta name="twitter:image" content={window.location.origin+'/icons/logo-152.png'} />
+<meta name="twitter:creator" content="@DavidWShadow" />
+<meta property="og:type" content="website" />
+<meta property="og:title" content="OtoplusID" />
+<meta property="og:description" content="Jual Mobil Daihatsu Dan Dapatkan Reward!" />
+<meta property="og:site_name" content="OtoplusID" />
+<meta property="og:url" content={window.location.origin} />
+<meta property="og:image" content={window.location.origin+'/icons/logo-152.png'} />
+
+        </Head>
       <body>
         <Main />
         <NextScript />
