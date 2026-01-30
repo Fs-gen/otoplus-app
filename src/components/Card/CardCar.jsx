d628ae521822aeb5353b5da976cc039451d869a7
MuhAdnanCoding <adnanadjah456@gmail.com>
2026-01-30 21:05:29 +0800

    fix cardcar

--- src/components/Card/CardCar.jsx
+++ src/components/Card/CardCar.jsx
@@ -11,7 +11,10 @@ const CardCar = ({ props }) => {
       <div className="flex flex-col gap-2 p-4 min-h-34 md:min-h-max justify-between">
         <div className="">
           <h1 className="font-semibold">{props.nama}</h1>
-          <div className="text-[11px] font-semibold">{props.harga_mulai}</div>
+          <div className="text-[11px] font-semibold">
+            <h1>Mulai Dari</h1>
+            <h1>{props.harga_mulai}</h1>
+          </div>
         </div>
         <Link
           className="bg-blue-semi w-full py-2 text-[10px] font-semibold text-white rounded-md flex gap-1 items-center justify-center"
