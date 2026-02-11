f95462f92c8d064291bb6012142e9ab517e02120
Fs-Gen <49970510+Fs-gen@users.noreply.github.com>
2026-02-11 09:38:06 +0000

    perbarui teks judul pada komponen InputCopy untuk menambahkan kejelasan pada cara berbagi katalog

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -153,7 +153,7 @@ const Home = () => {
             />
             <div className="border-b-2 border-gray-200 mx-4"></div>
             <InputCopy
-              title="Bagikan katalog dan dapatkan reward dari setiap pembelian mobil!"
+              title="Bagikan katalog dan dapatkan reward dari setiap pembelian mobil melalu link ini!"
               desc="Link Katalog"
               props={referralOffers}
               id="share"
